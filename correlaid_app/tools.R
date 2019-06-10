4db8af533fe7431c3df076f6ff6999cdeae54691
Eva Martínez <3v4.mr6@gmail.com>
2019-06-10 12:31:34 +0200

    first working dashboard

--- correlaid_app/tools.R
+++ correlaid_app/tools.R
@@ -1,20 +1,69 @@
+library(tidyverse)
+library(plotly)
+library(readxl)
 
+###########################################
+
+fragen_dim_namen.df <- read_excel("exports/mapping_colnames_20190530_174209.xlsx", 
+                                               sheet = "fragen_dim_namen")
+
+start_question_choices <- fragen_dim_namen.df %>%
+      select(question_id_variable_name, bar_chart) %>% 
+      filter(bar_chart == TRUE) %>%
+      pull("question_id_variable_name") 
+
+col_names_labels.df <- read_excel("exports/mapping_colnames_20190530_174209.xlsx", 
+                                  sheet = "col_names_labels")
+
+vars <- str_split(start_question_choices[[1]], pattern = " ", simplify = TRUE)[ ,2]
+colnames_filtered_data_start <- data %>% 
+   select(starts_with(vars), -ends_with("other")) %>% colnames 
+
+start_variable_choices <- col_names_labels.df %>% 
+   filter(colnames %in% colnames_filtered_data_start) %>%
+   pull("collabels_short")
+
+######################################
+
+pasteCollPlus <- function(...) {
+   paste(..., collapse = "+")
+}
+
+create_barplot_data <- function(df){
+   total_ids <- df %>% 
+      gather(key = "variable", value = "answer", -id, na.rm = TRUE) %>% 
+      group_by(variable) %>% 
+      summarise(total_responses = length(unique(id)))
+   
+   summ.df <- df %>% 
+      gather(key = "variable", value = "answer", -id, na.rm = TRUE) %>% 
+      group_by(variable, answer) %>% 
+      summarise( responses = length(unique(id))) %>%
+      merge(total_ids, by = "variable") %>%
+      mutate(responses_percentage = round(100*responses/total_responses, 2))
+   
+   return(summ.df)
+}
 
 create_barplot <- function(df, percent = FALSE, title = ""){
-      summ.df <- df %>% 
-            gather(key = "question", value = "answer", -id, na.rm = TRUE) %>% 
-            group_by(question, answer) %>% 
-            summarise( responses = length(unique(id)))
+      
       if(percent){
-            summ.df %>% 
-                  ggplot(aes(x= question, y = responses, fill = answer)) + 
-                  geom_bar(stat = "identity", position = "fill") + #make the stacked bars
-                  scale_y_continuous(labels = scales::percent) + #change y axis to % format 
-                  theme(legend.position="bottom", axis.text.x = element_text(angle = 45, hjust = 1))    
+         df %>% 
+            ggplot(aes(x= variable, y = responses_percentage, fill = answer)) + 
+            geom_bar(stat = "identity", position = "dodge") + #make the stacked bars
+            theme(legend.position="bottom", axis.text.x = element_text(angle = 45, hjust = 1)) +
+            ggtitle(title) + 
+            xlab("Selected Variable(s)") +
+            ylab("% of responses")
+                  
       } else {
-            summ.df %>% 
-                  ggplot(aes(x= question, y = responses, fill = answer)) + 
-                  geom_bar(stat = "identity", position = "stack") + #make the stacked bars
-                  theme(legend.position="bottom", axis.text.x = element_text(angle = 45, hjust = 1))
+         df %>% 
+            ggplot(aes(x= variable, y = responses, fill = answer)) + 
+            geom_bar(stat = "identity", position = "dodge") + #make the stacked bars
+            theme(legend.position="bottom", axis.text.x = element_text(angle = 45, hjust = 1)) +
+            ggtitle(title) + 
+            xlab("Selected Variable(s)") +
+            ylab("Number of responses")
       }
 }
+
